52af264779e32d4e9568d86dd5f52c16925d9dd1
ntokom89 <59078280+ntokom89@users.noreply.github.com>
2022-05-10 14:39:39 +0200

    No comment

--- app/src/main/java/com/company/travelapp/MainActivity.java
+++ app/src/main/java/com/company/travelapp/MainActivity.java
@@ -4,18 +4,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Collection> listCollection;
+    ImageView goal,categories,camera, album, setGoal, addCategory, addItem, uploadImage, viewItem, personal;
+    EditText search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goal = findViewById(R.id.imageViewGoal);
+        categories = findViewById(R.id.imageViewCategories);
+        camera = findViewById(R.id.imageViewCamera);
+        album = findViewById(R.id.imageViewPhotoAlbum);
+        setGoal = findViewById(R.id.imageViewSetGoal);
+        addCategory = findViewById(R.id.imageViewAddCategory);
+        addItem = findViewById(R.id.imageViewAddItem);
+        uploadImage =findViewById(R.id.imageViewUploadImage);
+        viewItem = findViewById(R.id.imageViewViewItem);
+        personal = findViewById(R.id.imageViewPersonSettings);
+
+        search = findViewById(R.id.editTextSearchMain);
+
+        goal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,GoalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        categories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CategoryListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddGoalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
 
 
