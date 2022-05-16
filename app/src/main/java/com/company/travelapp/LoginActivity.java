e912c7a22984c3adb0740bbf9501f76490e54c00
ntokom89 <59078280+ntokom89@users.noreply.github.com>
2022-05-16 09:57:16 +0200

    Firebase authentication works for login and registration. User able to be added to database during registration.

--- app/src/main/java/com/company/travelapp/LoginActivity.java
+++ app/src/main/java/com/company/travelapp/LoginActivity.java
@@ -1,14 +1,74 @@
 package com.company.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText password, email;
+    Button buttonLogin;
+    FirebaseAuth mAuth;
+    TextView register;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        password = findViewById(R.id.editTextTextPassword);
+        email = findViewById(R.id.editTextEmailLogin);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        register = findViewById(R.id.textViewRegister);
+
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInWithAccount(email.getText().toString(), password.getText().toString());
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void signInWithAccount(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser userFirebase = mAuth.getCurrentUser();
+                    Toast.makeText(LoginActivity.this,"Login successful",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this,"Check your email or password",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/company/travelapp/RegisterActivity.java
+++ app/src/main/java/com/company/travelapp/RegisterActivity.java
@@ -1,14 +1,124 @@
 package com.company.travelapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    User user;
+    FirebaseAuth mAuth;
+    DatabaseReference databaseReference;
+
+    EditText username, email, password;
+    TextView login;
+    Button buttonRegister;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        user = new User();
+        username = findViewById(R.id.editTextUsernameRegister);
+        email = findViewById(R.id.editTextEmailAddressRegister);
+        password = findViewById(R.id.editTextTextPasswordRegister);
+        buttonRegister = findViewById(R.id.buttonRegisterUser);
+        progressBar = findViewById(R.id.progressBarRegister);
+        login = findViewById(R.id.textViewSignIn);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createAccount(email.getText().toString(),password.getText().toString(),username.getText().toString());
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+    private void createAccount(String email, String password, String username) {
+
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setUsername(username);
+
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser userFirebase = mAuth.getCurrentUser();
+                    Toast.makeText(RegisterActivity.this,"User Created",Toast.LENGTH_LONG).show();
+                    addToFirebase(user);
+                    Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    updateUI(userFirebase);
+
+                }else{
+                    Toast.makeText(RegisterActivity.this,"Registration Unsuccessful",Toast.LENGTH_LONG).show();
+                    updateUI(null);
+                }
+            }
+        });
+
+
+
+    }
+
+    private void updateUI(FirebaseUser userFirebase) {
+    }
+
+    private void addToFirebase(User user) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //databaseReference.setValue(email);
+                databaseReference.child(user.getUsername()).setValue(user);
+                //databaseReference.child("password").setValue(password);
+                Toast.makeText(RegisterActivity.this,"data added to database ",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //Toast.makeText(RegisterActivity.this,"Database error",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
